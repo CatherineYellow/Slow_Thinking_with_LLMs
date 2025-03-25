f6e7fb93b97b8130553318b5dd45320b97b2995c
CatherineYellow <huangjj2021@mail.sustech.edu.cn>
2025-03-25 10:28:31 +0000

    correct dataset

--- STILL-3-TOOL/evaluation/run_eval_tool.py
+++ STILL-3-TOOL/evaluation/run_eval_tool.py
@@ -30,9 +30,9 @@ random.seed(42)
 # AIME25: "STILL-3-TOOL/evaluation/dataset/AIME25.jsonl"
 # hmmt_feb_2025: "STILL-3-TOOL/evaluation/dataset/hmmt_feb_2025.jsonl"
 name2path = {
-    "AIME24": "STILL-3-TOOL/evaluation/dataset/AIME24.jsonl",
-    "AIME25": "STILL-3-TOOL/evaluation/dataset/AIME25.jsonl",
-    "hmmt_feb_2025": "STILL-3-TOOL/evaluation/dataset/hmmt_feb_2025.jsonl",
+    "AIME24": "dataset/AIME24.jsonl",
+    "AIME25": "dataset/AIME25.jsonl",
+    "hmmt_feb_2025": "dataset/hmmt_feb_2025.jsonl",
 }
 
 name2eval = {
